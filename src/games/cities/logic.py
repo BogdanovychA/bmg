e8c15ac3798e43b61a6295fd4be37d35690a6beb
Andrii Bogdanovych <a@bogdanovych.org>
2026-02-17 17:02:31 +0200

    Функція створення очищеної бази міст

--- src/games/cities/logic.py
+++ src/games/cities/logic.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
+type CityStorage = dict[str, set[str]]
+type UsedCities = set[str]
+
 from .database.blacklists import NORMALISED_BLACKLIST
 from .database.normalised import NORMALISED
 
+
+def create_available_cities() -> CityStorage:
+    return {
+        letter: (cities - NORMALISED_BLACKLIST.get(letter, set()))
+        for letter, cities in NORMALISED.items()
+    }
+
+
 if __name__ == "__main__":
-    print("NORMALISED_BLACKLIST:", NORMALISED_BLACKLIST)
-    print("NORMALISED:", NORMALISED)
+    available_cities: CityStorage = create_available_cities()
+    used_cities: UsedCities = set()
+
+    print("available_cities:")
+    for key, value in available_cities.items():
+        print(f"{key}: {value}")
