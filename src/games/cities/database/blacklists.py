6f0de96cac3e622e8f1c58949afc74f0db1e8c97
Andrii Bogdanovych <a@bogdanovych.org>
2026-02-17 15:09:30 +0200

    Чистка бази міст. Додано блекліст (ще не наповнено)

---
+++ src/games/cities/database/blacklists.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+blacklist_letters = {
+    "и",
+    "ь",
+}
+
+blacklist_symbols = {
+    "'",
+    "-",
+    " ",
+}
+
+blacklist_cities = {
+    "м": {
+        "Москва",
+        "Мінськ",
+    },
+}
